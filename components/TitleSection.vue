0103e992c73f8b217c4033d27baf4c58699105de
Mykhailo Melikov <coutaq@gmail.com>
2025-06-17 19:55:49 +0200

    upgraded to nuxt v4

--- components/TitleSection.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<section class="col-span-1">
-    <NuxtImg src="/images/me.jpg" alt="A nice picture of me." width="300" height="300" :placeholder="[300, 300, 50, 5]" quality="80" preload ></NuxtImg>
-</section>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  compatibilityVersion: 4,
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  css: ["~/app/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
   modules: [
-    "@nuxt/content",
     "@nuxt/eslint",
     "@nuxt/fonts",
     "@nuxt/icon",
